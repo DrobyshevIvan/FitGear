99518104fd1b9376efe44a4e3c5a443ae807fdb2
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2025-05-30 19:48:01 +0300

    Added list of categories and "Add category"
    Now page "Manage Categories" shows list of all categories. Also "Add Category" button was added, so now Admin can add Categories by their name.

--- newFrontend/fitgear/src/pages/CategoriesManage.jsx
+++ newFrontend/fitgear/src/pages/CategoriesManage.jsx
@@ -1,3 +1,49 @@
+import { useState, useEffect } from "react";
+import AddCategory from "./addCategory";
+import { addCategory, getAllCategories } from "../services/categories";
+
 export default function CategoriesManage() {
-    return <div>Categories Manage</div>
+    const [categories, setCategories] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        getAllCategories().then(data => {
+            console.log("Изначальные категории:", data);
+            setCategories(data.$values || []);
+        });
+    }, []);
+
+    const handleAdd = async (data) => {
+        await addCategory(data);
+        setIsModalOpen(false);
+        const updated = await getAllCategories();
+        console.log("Данные с бэкенда:", updated);
+        setCategories(Array.isArray(updated.$values) ? updated.$values : []);
+    };
+
+    return (
+        <div className="p-6">
+            <button
+                onClick={() => setIsModalOpen(true)}
+                className="flex items-center gap-2 text-white bg-blue-600 px-4 py-2 rounded shadow hover:bg-blue-700 mb-4"
+            >
+                + Add Category
+            </button>
+
+            <AddCategory
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onSubmit={handleAdd}
+            />
+
+            <ul className="space-y-2">
+                {categories.map(c => (
+                <li key={c.id} className="border p-2 flex justify-between items-center">
+                    <span>{c.name}</span>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
+
 }
---
+++ newFrontend/fitgear/src/services/categories.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5209/api/Category";
+
+export const getAllCategories = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
+
+export const addCategory = async (category) => {
+    const response = await axios.post(API_URL, category);
+    return response.data;
+};
