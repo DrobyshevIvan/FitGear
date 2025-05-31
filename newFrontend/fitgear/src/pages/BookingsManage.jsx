3302862eedb64d76c03942a3943ec31fd163bb05
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2025-05-31 23:34:17 +0300

    Added filtration by status
    Now users can filter bookings by their statuses, using a drop-down menu (STC).

--- newFrontend/fitgear/src/pages/BookingsManage.jsx
+++ newFrontend/fitgear/src/pages/BookingsManage.jsx
@@ -5,6 +5,7 @@ import { getAnnouncementById } from "../services/anouncements";
 
 export default function BookingsManage() {
     const [bookings, setBookings] = useState([]);
+    const [filterStatus, setFilterStatus] = useState("All");
 
     /*const NormalizeBookings = (data) => {
         const array = Array.isArray(data) ? data : data?.$values;
@@ -43,12 +44,24 @@ export default function BookingsManage() {
         fetchData();
     }, []
     );
+
+    const filteredBookings = bookings.filter(b => 
+        filterStatus === "All" || b.status === filterStatus
+    );
     
     return (
         <div>
+            <select
+                value={filterStatus}
+                onChange={(e) => setFilterStatus(e.target.value)}
+                className="mb-4 border px-2 py-1 rounded"
+            >
+                <option value="All">All</option>
+                <option value="Pending">Pending</option>
+                <option value="Approved">Approved</option>
+            </select>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-                {bookings
-                .filter(c => c.status === "Pending")
+                {filteredBookings
                 .map(c => (
                     <div key={c.id} className="border p-4 rounded shadow bg-white flex flex-col justify-between">
                         <h1>{c.announcement.title}</h1>
