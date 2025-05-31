7afbb80ad1391fda5c1daf0d4e6f8781e881ea29
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2025-05-31 22:12:31 +0300

    Added list of bookings
    Now users can see the list of bookings on the Manage Bookings page.

--- newFrontend/fitgear/src/pages/BookingsManage.jsx
+++ newFrontend/fitgear/src/pages/BookingsManage.jsx
@@ -1,7 +1,62 @@
 import { useState, useEffect } from "react";
+import { getAllBookings } from "../services/bookings";
+import { getAnnouncementById } from "../services/anouncements";
+
 
 export default function BookingsManage() {
+    const [bookings, setBookings] = useState([]);
+
+    /*const NormalizeBookings = (data) => {
+        const array = Array.isArray(data) ? data : data?.$values;
+        if (!array) return [];
+        
+        return array.map(c => ({
+            ...c,
+            id: c.id ?? parseInt(c.$id),
+            announcementName: getAnnouncementById(c.announcementId)
+        }));
+    }*/
+
+    useEffect(() => {
+        /*getAllBookings().then(data => {
+            const normalized = NormalizeBookings(data);
+            console.log("Bookings list:", normalized);
+            setBookings(normalized);
+        })*/
+
+        const fetchData = async () => {
+            const rawBookings = await getAllBookings();
+
+            const enriched = await Promise.all(
+                rawBookings.map(async (c) => {
+                    const announcement = await getAnnouncementById(c.announcementId);
+                    return {
+                        ...c,
+                        id: c.id ?? parseInt(c.$id),
+                        announcement: announcement
+                    };
+                })
+            )
+            console.log("Bookings:", enriched);
+            setBookings(enriched);
+        };
+        fetchData();
+    }, []
+    );
+    
     return (
-        <h1>Bookings Manage</h1>
+        <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                {bookings
+                .filter(c => c.status === "Pending")
+                .map(c => (
+                    <div key={c.id} className="border p-4 rounded shadow bg-white flex flex-col justify-between">
+                        <h1>{c.announcement.title}</h1>
+                        <h1>{c.status}</h1>
+                    </div>
+                ))
+                }
+            </div>
+        </div>
     )
 }
